da5582c7e9e6529c6e2f9d4914c69a4876a6b06f
jolathesurvivor <62813630+jolathesurvivor@users.noreply.github.com>
2021-09-05 15:42:11 +0300

    Update foodItem.js
    should work

--- foodItem.js
+++ foodItem.js
@@ -5,29 +5,52 @@ class foodItem {
         this.price = price
         this.happiness = happiness
         this.texture = texture
+        xpos
+        ypos
         foods.push(this)                    //adds food to the list of foods
     }
     showPrice = function() {                //returns the price as a string, with a $ prepended
         return "$" + this.price.toString
     }
-    showPos = function() {                  //returns the coordinates of the item (provided there are coordinates for it)
-        return mapped.get(this)
+    setPos = function(x, y) {
+        xpos = x
+        ypos = y
     }
 }
 
-class coordinates {
-    constructor(xCoord, yCoord) {           //constructor, one x coord, and one y coord
-        this.x = xCoord
-        this.y = yCoord
-        coords.push(this)                   //adds coordinates to the list of coordinates
-    }
-
-}
-
-coords = new Array
 foods = new Array
 
-mapped = new Map                            //keeps track of coordinates of all foods
-mapItem = function(food, co) {              //maps a food item to a set of coordinates
-    mapped.set(food, co)
+class Shelf {
+    constructor(vertical, size) {
+        x
+        y
+        width
+        heigth
+        slots = new Array
+        slotcoordx = new Array
+        slotcoordy = new Array
+        if (vertical) {
+            width = 32 * size
+            heigth = 32
+        } else {
+            width = 32
+            heigth = 32 * size
+        }
+    }
+    position = function(xpar, ypar) {
+        x = xpar
+        y = ypar
+    }
+    placeItem = function(food) {
+        if (this.slots.size < this.size) {
+            this.slots.push(food)
+            if (this.vertical) {
+                food.xpos = this.x + 16
+                food.ypos = this.y + 16 + 32*(slots.size - 1)
+            } else {
+                food.xpos = this.x + 16 + 32*(slots.size - 1)
+                food.ypos = this.y + 16
+            }
+        }
+    }
 }
