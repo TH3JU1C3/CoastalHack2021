49893e2e03b1e79966b6dc4901c1d53f4990b962
jolathesurvivor <62813630+jolathesurvivor@users.noreply.github.com>
2021-09-05 17:53:10 +0300

    Update foodItem.js
    Updated shelves to be easier to work in the grid

--- foodItem.js
+++ foodItem.js
@@ -18,37 +18,19 @@ foods = new Array                           //an array listing all the foods tha
 shelves = new Array                         //an array listing all the shelves that have been made
 
 class Shelf {
-    constructor(vertical, size) {           //verticality is signified by a Boolean. True means the shelf should be vertically aligned, and false horizontally.
-        this.x = 0                          //size refers to how many 32x32 slots the shelf uses. each slot provides room for one food item
-        this.y = 0
+    constructor(x, y) {           
+        this.x = x                        
+        this.y = y
         this.width = 32
         this.heigth = 32
-        this.size = size
-        this.slots = new Array              //an Array showing which foods are placed on the shelf
-        if (vertical) {                     //checks if it's horizontal or vertical, and adjusts heigth/width accordingly
-            this.width = 32
-            this.heigth = 32 * size
-        } else {
-            this.width = 32 * size
-            this.heigth = 32
-        }
         shelves.push(this)
-    }
-    position = function(xpar, ypar) {       //sets the position of the shelf. presumably where the top-left corner of the shelf is.
-        this.x = xpar
-        this.y = ypar
+        this.hasFood = Boolean(false)
     }
     placeItem = function(food) {            
-        if (this.slots.length < this.size) {        //can't place in a full shelf
-            this.slots.push(food)                   //adds the food to the next slot
-            if (this.vertical) {                    //checks vertical/horizontal
-                food.xpos = this.x 
-                food.ypos = this.y + 32*(this.slots.length - 1)
-            } else {
-                food.xpos = this.x + 32*(this.slots.length - 1)
-                food.ypos = this.y
-            }
+        if (!hasFood) {
+            food.xpos = this.x
+            food.ypos = this.y
+            this.hasFood = Boolean(true)
         }
     }
-
 }
