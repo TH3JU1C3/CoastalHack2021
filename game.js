554d869e15b2da37e264278d254d6ab5f14d813d
MethLabrador <62994899+MethLabrador@users.noreply.github.com>
2021-09-05 18:32:01 +0200

    comments and movement changes

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 const mapLevel = document.querySelector("canvas").getContext("2d");
 height = 448;
 width = 1024;
-
+foods = new Array;
+shelves = new Array;
 mapLevel.canvas.height = height;
 mapLevel.canvas.width = width;
+currentLevel = 0; //debug purpose
 
 //Makes floor
 const makeFloor = function(ctx)
@@ -13,11 +15,40 @@ const makeFloor = function(ctx)
     ctx.fillStyle = pattern;
     ctx.fillRect(0,0,width, height);
 }
-const buildShelves = function(ctx)
+
+//Paints shelves from the level array
+//Should support more levels!
+const buildShelves = function(ctx, level)
+{
+    //coords
+    shelfX = 0
+    shelfY = 0
+    //Iterating throught the level and drawing the shelves
+    for (i = 0; i < levels[level].length; i ++)
+    {
+        shelfX = 0; //reset x coord back to 0
+        for (j = 0; j < levels[0][0].length; j++)
+        {
+            shelf = levels[level][i][j]
+            if (shelf)
+            {
+                mapLevel.fillStyle = "#EC7C5F"; // hex for shelf color
+                mapLevel.beginPath();
+                mapLevel.rect(shelfX, shelfY, 32, 32);
+                mapLevel.fill();
+            }
+            shelfX += 32;
+        }
+        shelfY += 32;
+    }
+}
+
+//Only purpose is to load shelf objects
+const buildShelves2 = function(ctx, level)
 {
     shelfX = 0
     shelfY = 0
-    for (i = 0; i < levels[0].length; i ++)
+    for (i = 0; i < levels[level].length; i ++)
     {
         shelfX = 0;
         for (j = 0; j < levels[0][0].length; j++)
@@ -29,6 +60,7 @@ const buildShelves = function(ctx)
                 mapLevel.beginPath();
                 mapLevel.rect(shelfX, shelfY, 32, 32);
                 mapLevel.fill();
+                new Shelf(shelfX, shelfY);
             }
             shelfX += 32;
         }
@@ -36,7 +68,34 @@ const buildShelves = function(ctx)
     }
 }
 
-
+class Shelf {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 32;
+        this.heigth = 32;
+        shelves.push(this);
+        this.hasFood = Boolean(false);
+        this.placeItem = function (food) {
+            if (!this.hasFood) {
+                food.xpos = this.x;
+                food.ypos = this.y;
+                this.hasFood = Boolean(true);
+            }
+        };
+        const FoodItem = function (name, price, cals) {
+            this.name = name;
+            this.price = price;
+            this.cals = cals;
+            this.xpos = 0;
+            this.ypos = 0;
+            foods.push(this); //adds food to the list of foods
+            this.showPrice = function () {
+                return "$" + this.price.toString;
+            };
+        };
+    }
+}
 const levels = [[
 	[0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0],
 	[0,0,1,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0],
@@ -97,7 +156,6 @@ const player = {
     }
 };
 
-
 const controller = {
 
   left: false,
@@ -131,6 +189,8 @@ const controller = {
 };
 
 const loop = function () {
+
+
     speed = 0.25;
     if (controller.up) {
             player.yVelocity -= speed;
@@ -147,8 +207,8 @@ const loop = function () {
 
     player.x += player.xVelocity;// movement
     player.y += player.yVelocity;// movement
-    player.xVelocity *= 0.9;// friction
-    player.yVelocity *= 0.9;// friction
+    player.xVelocity *= 0.7;// friction
+    player.yVelocity *= 0.7;// friction
 
     // if square is falling below floor line
     if (player.y >= height - player.width) {
@@ -175,7 +235,7 @@ const loop = function () {
   }
   
     makeFloor(mapLevel);  
-    buildShelves(mapLevel);  
+    buildShelves(mapLevel, currentLevel);  
     player.draw(mapLevel);
     
 
@@ -184,6 +244,9 @@ const loop = function () {
 
 };
 
-window.addEventListener("keydown", controller.keyListener)
+//builds shelf objects 
+buildShelves2(mapLevel, currentLevel);
+
+window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
 window.requestAnimationFrame(loop);
