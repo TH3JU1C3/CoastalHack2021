0be3bbc4f81dd8cc75c535aa8cb90ca84b95f614
jolathesurvivor <jolathesurvivor@gmail.com>
2021-09-06 17:29:21 +0300

    Update game.js

--- game.js
+++ game.js
@@ -19,8 +19,22 @@ currentLevel = 0; //debug purpose
 foodString = ["bacon", "cheese", "bbq", "cookies", "chips", "soda", "wine", "banana", "pepper", "cabbage", "fish", "apple", "milk", "beef"]
 
 //random starting values
-money = 100
+money = 10000000
 calories = 0
+
+//collision detection functions
+const shelfcolleft = function(curshelf) {
+    return ((player.x + 30) < (curshelf.x + 16) && (player.x + 30) > curshelf.x) && (((player.y + 16) < (curshelf.y + 16) && (player.y + 16) > curshelf.y) || (player.y + 16 > (curshelf.y + 16) && player.y + 16 < (curshelf.y + 30)))
+}
+const shelfcolright = function(curshelf) {
+    return (player.x > (curshelf.x + 16) && player.x < (curshelf.x + 30)) && (((player.y + 16) < (curshelf.y + 16) && (player.y + 16) > curshelf.y) || (player.y + 16 > (curshelf.y + 16) && player.y + 16 < (curshelf.y + 30)))
+}
+const shelfcolup = function(curshelf) {
+    return ((player.y + 30) < (curshelf.y + 16) && (player.y + 30) > curshelf.y) && ((player.x + 16 > (curshelf.x + 16) && player.x + 16 < (curshelf.x + 30)) || ((player.x + 16) < (curshelf.x + 16) && (player.x + 16) > curshelf.x))
+}
+const shelfcoldown = function(curshelf) {
+    return (player.y > (curshelf.y + 16) && player.y < (curshelf.y + 30)) && (((player.x + 16) < (curshelf.x + 16) && (player.x + 16) > curshelf.x) || (player.x + 16 > (curshelf.x + 16) && player.x + 16 < (curshelf.x + 30)))
+}
 
 const distance = function(a, b) {       //a is the player, b is the food. calculates the distance between the two, and returns said distance
     x = Math.abs(a.x - b.xpos)
@@ -300,7 +314,22 @@ const loop = function () {
 
         player.x = width - player.width;
         player.xVelocity = 0;
-  }
+    }
+    for(i = 0; i < shelves.length; i++) {   //if player collides with a shelf
+        const curshelf = shelves[i]
+        if (shelfcolleft(curshelf)) {
+            player.xVelocity = -1
+        }
+        else if (shelfcolright(curshelf)) {
+            player.xVelocity = 1
+        }
+        else if (shelfcolup(curshelf)){
+            player.yVelocity = -1
+        }
+        else if (shelfcoldown(curshelf)){
+            player.yVelocity = 1
+        }
+    }
   
     //Drawing all the important stuff
     makeFloor(mapLevel);  
