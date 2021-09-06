27421a3d830e5c2cd0251aac0b0c469ff1e0489a
jolathesurvivor <jolathesurvivor@gmail.com>
2021-09-06 18:48:00 +0300

    Merge branch 'main' of https://github.com/TH3JU1C3/CoastalHack2021 into main

--- game.js
+++ game.js
@@ -345,7 +345,24 @@ buildShelves2(mapLevel, currentLevel);
 
 putFood();
 
-
+shuffle = function(array) {
+    var currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  foods = shuffle(foods)
 //add food to shelves
 shelves.map(x => x.placeItem(foods[shelves.indexOf(x)]))
 
